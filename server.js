46a232149d72f1d160ef3ab7e564df2413221486
cmfyoung <cmfyoung7@gmail.com>
2024-01-04 20:54:27 -0500

    Further build out server

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+);
 function onStart() {
-    inquirer.prompt ({
+    inquirer.prompt({
         type: "list",
         name: "action",
         message: "What would you like to do?",
@@ -15,51 +32,61 @@ function onStart() {
             "Add a Manager",
             "Update an employee role",
             "View Employees by Manager",
-            "View Employees by Department", 
+            "View Employees by Department",
             "Delete Departments | Roles | Employees",
-            "View the total utilized budget of a department", 
+            "View the total utilized budget of a department",
             "Exit"
         ],
     })
-    .then ((answer) => {
-        switch (answer.action) {
-            case "View all departments":
-                viewAllDepartments();
-                break;
-            case  "View all roles":
-                viewAllRoles();
-                break;
-            case "Add a department":
-                addDepartment();
-                break;
-            case "Add a role":
-                addRole();
-                break;
-            case "Add an employee":
-                addEmployee();
-                break;
-            case "Add a Manager":
-                addManger();
-                break;
-            case "Update an employee role":
-                updateRole();
-                break;
-            case "View Employees by Manager":
-                viewEmployeesByManager();
-                break;
-            case "View Employees by Department":
-                viewEmployeesByDepartment();
-                break; 
-            case "Delete Departments | Roles | Employees":
-                deleteDRE();
-                break;
-            case "View the total utilized budget of a department": 
-                viewTotalBudge();
-                break;
-            case "Exit":
-                connection.end();
-                console.log("Bye");
-                break;
-        }
-    })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Add a Manager":
+                    addManger();
+                    break;
+                case "Update an employee role":
+                    updateRole();
+                    break;
+                case "View Employees by Manager":
+                    viewEmployeesByManager();
+                    break;
+                case "View Employees by Department":
+                    viewEmployeesByDepartment();
+                    break;
+                case "Delete Departments | Roles | Employees":
+                    deleteDRE();
+                    break;
+                case "View the total utilized budget of a department":
+                    viewTotalBudge();
+                    break;
+                case "Exit":
+                    connection.end();
+                    console.log("Bye");
+                    break;
+            }
+        })
 }
+
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
